3427d1ae7c946d04a12c6798faf66d374f2539e1
sun1zu <kiri.andreev2006@yandex.ru>
2025-05-18 15:13:35 +0300

    add player sprite, wip rotation

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,14 +6,43 @@ namespace RECOVER.Scripts;
 
 public class PlayerController : Component
 {
+    private float rotationSpeed = 180f; // degrees per second
+    private float moveSpeed = 1f;
+    private bool rotateLeft, rotateRight;
+    
     public override void Update(double deltaTime)
     {
+        // Get input
         Vector input = new Vector();
         if (Keyboard.IsKeyDown(Key.W)) input.Y -= 1;
         if (Keyboard.IsKeyDown(Key.S)) input.Y += 1;
         if (Keyboard.IsKeyDown(Key.A)) input.X -= 1;
         if (Keyboard.IsKeyDown(Key.D)) input.X += 1;
+        
+        if (Keyboard.IsKeyDown(Key.E)) rotateRight = true;
+        if (Keyboard.IsKeyDown(Key.R)) rotateLeft = true;
+        if(Keyboard.IsKeyUp(Key.E)) rotateRight = false;
+        if (Keyboard.IsKeyDown(Key.R)) rotateLeft = false;
+        
+        // Apply rotation
+        if (rotateLeft)
+            GameObject.Transform.Rotation = new Vector(
+                GameObject.Transform.Rotation.X,
+                GameObject.Transform.Rotation.Y - (float)(rotationSpeed * deltaTime)
+            );
+        if (rotateRight)
+            GameObject.Transform.Rotation = new Vector(
+                GameObject.Transform.Rotation.X,
+                GameObject.Transform.Rotation.Y + (float)(rotationSpeed * deltaTime)
+            );
 
-        GameObject.Transform.Velocity += input * 50 * deltaTime;
+        // Apply movement relative to rotation
+        double angle = GameObject.Transform.Rotation.Y * (float)Math.PI / 180f;
+        Vector rotatedInput = new Vector(
+            input.X * (float)Math.Cos(angle) - input.Y * (float)Math.Sin(angle),
+            input.X * (float)Math.Sin(angle) + input.Y * (float)Math.Cos(angle)
+        );
+        
+        GameObject.Transform.Velocity += rotatedInput * moveSpeed;
     }
 }
