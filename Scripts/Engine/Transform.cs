b3c0692c8bc6da90c1d57443a3c875b2f9590aa1
Kirill Andreev <kiri.andreev2006@yandex.ru>
2025-05-18 16:17:36 +0300

    Player rotation and sprite (#11)
    * add player sprite, wip rotation
    
    * add camera and player rotation, wip
    
    * done rotation and sprite
    
    * done rotation and sprite
    
    * PlayerMen Image to App.Resources
    
    ---------
    
    Co-authored-by: Bill44-NotAlone <denyc9@mail.ru>

--- Scripts/Engine/Transform.cs
+++ Scripts/Engine/Transform.cs
@@ -7,6 +7,7 @@ public class Transform : DeafNotificationObject
 {
     private Vector _position = new Vector(0, 0);
     private Vector _velocity = new Vector(0, 0);
+    private Vector _rotation = new Vector(0, 0);
 
     public Vector Position
     {
@@ -18,5 +19,11 @@ public class Transform : DeafNotificationObject
     {
         get => _velocity;
         set => Set(ref _velocity, value);
+    }
+
+    public Vector Rotation
+    {
+        get => _rotation;
+        set => Set(ref _rotation, value);
     }
 }
