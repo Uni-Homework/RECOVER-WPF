4602ded29dc9a5430d75f9e01fe7aaaf8056cc99
Bill44-NotAlone <denyc9@mail.ru>
2025-05-19 19:48:47 +0300

    TerminalView

--- Assets/Prefabs/Terminal/TerminalComponent.cs
+++ Assets/Prefabs/Terminal/TerminalComponent.cs
@@ -1,10 +1,21 @@
-﻿using RECOVER.Engine.Components;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using RECOVER.Assets.Scenes.Terminal;
+using RECOVER.Engine.Components;
 
 namespace RECOVER.Assets.Prefabs.Terminal;
 
 public class TerminalComponent : Component
 {
     private bool _isShow;
+    private ObservableCollection<string> history;
+    private string command;
+
+    public TerminalComponent()
+    {
+        history = new ObservableCollection<string>();
+        command = String.Empty;
+    }
 
     public bool IsShow
     {
@@ -12,8 +23,45 @@ public class TerminalComponent : Component
         private set => Set(ref _isShow, value);
     }
 
+    public ObservableCollection<string> History
+    {
+        get => history;
+        set { Set(ref history, value); }
+    }
+
+    public string Command
+    {
+        get => command;
+        set { Set(ref command, value); }
+    }
+
     public void OpenTerminal()
     {
+        if (IsShow)
+        {
+            return;
+        }
+
         IsShow = true;
+        TerminalView view = new TerminalView(this);
+        view.Closed += OnClosedTerminal;
+        view.Show();
+    }
+
+    private void OnClosedTerminal(object sender, EventArgs e)
+    {
+        (sender as Window).Closed -= OnClosedTerminal;
+        ClearCommand();
+        IsShow = false;
+    }
+
+    private void ClearCommand()
+    {
+        Command = String.Empty;
+    }
+
+    public void DoCommand()
+    {
+        History.Add(Command);
     }
 }
---
+++ Assets/Scenes/Terminal/TerminalView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using RECOVER.Assets.Prefabs.Terminal;
+
+namespace RECOVER.Assets.Scenes.Terminal;
+
+public partial class TerminalView : Window
+{
+    private TerminalComponent dataContext;
+
+    public TerminalView(TerminalComponent dataContext)
+    {
+        DataContext = this.dataContext = dataContext;
+        InitializeComponent();
+    }
+
+    private void ClickEnter(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            dataContext.DoCommand();
+        }
+    }
+}
