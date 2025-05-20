2502cef4caf620054c450d0ecd74a91aa1360ed9
sun1zu <kiri.andreev2006@yandex.ru>
2025-05-20 12:10:34 +0300

    koso krivo lish bi zhivo

--- Assets/Prefabs/Terminal/TerminalPrefab.cs
+++ Assets/Prefabs/Terminal/TerminalPrefab.cs
@@ -2,15 +2,20 @@
 using System.Windows.Input;
 using System.Windows.Media;
 using RECOVER.Assets.Prefabs.Item;
+using RECOVER.Assets.Prefabs.Player;
+using RECOVER.Engine;
 using RECOVER.Engine.Components;
 
 namespace RECOVER.Assets.Prefabs.Terminal;
 
 public class TerminalPrefab : ItemPrefab
 {
-    public TerminalPrefab(double x, double y) :
+    public GameObject Player { get; private set; }
+    
+    public TerminalPrefab(double x, double y, PlayerPrefab player) :
         base(x, y, 30, 50, Key.T, null, "Открыть терминал")
     {
+        Player = player;
         Transform.Origin = new Point(0.5, 1);
         TerminalComponent component = new TerminalComponent();
         Action = component.OpenTerminal;
