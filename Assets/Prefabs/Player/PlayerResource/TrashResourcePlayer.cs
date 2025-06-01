0c3fbadff32416f596015efccf8562ffbe09c082
Bill44 <denyc9@mail.ru>
2025-06-02 00:50:53 +0300

    add trash as resource

---
+++ Assets/Prefabs/Player/PlayerResource/TrashResourcePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media;
+using RECOVER.Engine.Components;
+
+namespace RECOVER.Assets.Prefabs.Player.PlayerResource;
+
+public class TrashResourcePlayer : Component, IResourcePlayer
+{
+    private double _current;
+    private double _max = 50;
+
+    public double Max
+    {
+        get => _max;
+        private set => Set(ref _max, value);
+    }
+
+    public double Min { get; } = 0;
+    public string Name { get; } = "Мусор";
+
+    public double Current
+    {
+        get => _current;
+        set
+        {
+            Set(ref _current, value);
+            if (_current > _max)
+            {
+                Max = Max + Max / 2;
+            }
+        }
+    }
+
+    public ImageSource ImageSource { get; }
+}
