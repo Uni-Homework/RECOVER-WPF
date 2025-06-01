7993dbe8f4deba3d382ed2510eaf2a0f8d6ebd02
Bill44 <denyc9@mail.ru>
2025-06-01 23:49:32 +0300

    Size Changer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 
 namespace RECOVER
 {
@@ -7,9 +8,23 @@ namespace RECOVER
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isNormalScreen;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.F11)
+            {
+                _isNormalScreen = !_isNormalScreen;
+                WindowStyle = _isNormalScreen ? WindowStyle.SingleBorderWindow : WindowStyle.None;
+                WindowState = _isNormalScreen ? WindowState.Normal : WindowState.Maximized;
+            }
+
+            base.OnKeyDown(e);
         }
     }
 }
