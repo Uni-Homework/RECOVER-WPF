62bf7315c4c952d33a7a6bb6b48b56f285629448
Bill44-NotAlone <denyc9@mail.ru>
2025-06-03 09:17:47 +0300

    styles for app

--- Assets/Scenes/GameOver/GameOverScene.cs
+++ Assets/Scenes/GameOver/GameOverScene.cs
@@ -15,12 +15,10 @@ public class GameOverScene : Scene
         Message = isWin ? "Победа!" : "Поражение!";
         
         RestartCommand = new LambdaCommand<object, object>(
-            _ => App.SetScene(SceneType.CosmicStation),
-            _ => true);
+            _ => App.SetScene(SceneType.CosmicStation));
             
         MainMenuCommand = new LambdaCommand<object, object>(
-            _ =>  App.SetScene(SceneType.MainMenu),
-            _ => true);
+            _ =>  App.SetScene(SceneType.MainMenu));
     }
 
     public string Message
