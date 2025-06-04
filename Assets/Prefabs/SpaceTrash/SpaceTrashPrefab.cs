9b5eada99c376782d22aa475dd3e4e0f814522ee
sun1zu <kiri.andreev2006@yandex.ru>
2025-06-04 22:44:51 +0300

    add +300fps, non-intersectional debris spawning

--- Assets/Prefabs/SpaceTrash/SpaceTrashPrefab.cs
+++ Assets/Prefabs/SpaceTrash/SpaceTrashPrefab.cs
@@ -26,4 +26,25 @@ public class SpaceTrashPrefab : ItemPrefab
         Transform.Position = new Vector(-9999, -9999);
         _fillerComponent.Replenish();
     }
+
+    public bool Intersects(Point p)
+    {
+        var width = GetComponent<BoxCollider>().Width;
+        var height = GetComponent<BoxCollider>().Height;
+        bool c1, c2;
+        c1 = c2 = false;
+        if (Transform.Position.X <= p.X && p.X <=Transform.Position.X + width) c1 = true;
+        if (Transform.Position.Y <= p.Y && p.Y <=Transform.Position.Y + height) c2 = true;
+        return c1 && c2;
+    }
+
+    public bool Intersects(SpaceTrashPrefab p)
+    {
+        foreach (var point in p.GetComponent<BoxCollider>().GetBoundsPoints())
+        {
+            if (Intersects(point)) return true;
+        }
+
+        return false;
+    }
 }
