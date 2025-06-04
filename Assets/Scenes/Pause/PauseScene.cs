58df2e2f7e938763d12fdc95163a7985fa080a3d
sun1zu <kiri.andreev2006@yandex.ru>
2025-06-04 15:33:22 +0300

    add tutorial button to pause menu

--- Assets/Scenes/Pause/PauseScene.cs
+++ Assets/Scenes/Pause/PauseScene.cs
@@ -9,11 +9,13 @@ public class PauseScene : Scene
     private ICommand _continuedCommand;
     private ICommand _toMainScreenCommand;
     private ICommand _exitCommand;
+    private ICommand _tutorialCommand;
 
     public PauseScene()
     {
         ContinuedCommand = new LambdaCommand<object, object>(ContinuedExecute);
         ToMainScreenCommand = new LambdaCommand<object, object>(_ => App.SetScene(SceneType.MainMenu));
+        TutorialCommand = new LambdaCommand<object, object>(_ => App.SetScene(SceneType.Learning));
         ExitCommand = new LambdaCommand<object, object>(_ => App.Current.Shutdown(0));
     }
 
@@ -33,6 +35,12 @@ public class PauseScene : Scene
     {
         get => _toMainScreenCommand;
         private set => Set(ref _toMainScreenCommand, value);
+    }
+
+    public ICommand TutorialCommand
+    {
+        get => _tutorialCommand;
+        private set => Set(ref _tutorialCommand, value);
     }
 
     public void ContinuedExecute(object _)
